80c9d67cc7c4d1b59cb092b99d5ed6e3c498ab01
dhruvaaArya05 <dhruvaa866@gmail.com>
2025-04-20 14:02:16 +0530

    third-commit

--- econest/app.js
+++ econest/app.js
@@ -22,6 +22,11 @@ const server = http.createServer(app);
 app.use(express.urlencoded());
 
 // app.use((req, res, next) => {
+//   console.log(req.url, req.method);
+//   next();
+// });
+
+// app.use((req, res, next) => {
 //   req.isLoggedIn = req.get('Cookie')?.split('=')[1] || false;
 //   next();
 // });
@@ -40,14 +45,17 @@ app.use(storeRouter);
 app.use(hostRouter);
 
 
+// mongodb://dhruvaa866:dhruvaadbroot%4005@ac-ds6tvzi-shard-00-00.z7yhero.mongodb.net:27017,ac-ds6tvzi-shard-00-01.z7yhero.mongodb.net:27017,ac-ds6tvzi-shard-00-02.z7yhero.mongodb.net:27017/?ssl=true&replicaSet=atlas-wv8qhm-shard-0&authSource=admin&retryWrites=true&w=majority&appName=CompleteCoding"
 
-const DB_PATH = "mongodb://localhost:27017/EcoShopDb";
+
+// const DB_PATH = "mongodb://localhost:27017/EcoShopDb";
 // const DB_PATH = "mongodb+srv://dhruvaa866:dhruvaadbroot@05@completecoding.z7yhero.mongodb.net/econestDb";
 
 const PORT = process.env.PORT || 3000;
-mongoose.connect(DB_PATH).then(() => {
+mongoose.connect("mongodb+srv://dhruvaa866:dhruvaadbroot%4005@completecoding.z7yhero.mongodb.net/econestDb?retryWrites=true&w=majority&appName=CompleteCoding").then(() => {
   server.listen(PORT, () => {
     console.log(`server running on address http://localhost:${PORT}`);
   });
 }).catch(err => {
+  // console.log('error connecting db', err);
 });
